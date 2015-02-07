525c729c8b0e5415ad2530d1e5c7c7cebee03ed1
Matt Conway <matt@classbug.com>
2015-02-06 23:00:25 -0600

    Initial Commit - full codebase

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,14 @@
+class LandingPagesController < ApplicationController
+  def home
+    
+  end
+  def about
+    respond_to :html
+  end
+  def charities
+    
+  end
+  def contact
+    
+  end
+end
